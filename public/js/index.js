1816b353612c2ed39998eefeaab7b71142adcaa3
Connor Durham <mittyman@gmail.com>
2019-09-28 09:51:13 -0400

    trying to fix CORS

--- public/js/index.js
+++ public/js/index.js
@@ -105,7 +105,7 @@ $("#submit-search").on("click", function(e) {
   var beer = $("#search-bar").val().trim();
 
   var queryUrl =
-    "https://sandbox-api.brewerydb.com/v2/beers?key=7aed8b39d23007c4acc04ff75f4a0d6e";
+    "https://cors-anywhere.herokuapp.com/https://sandbox-api.brewerydb.com/v2/beers?key=7aed8b39d23007c4acc04ff75f4a0d6e";
 
   console.log(beer);
 
@@ -116,3 +116,36 @@ $("#submit-search").on("click", function(e) {
     console.log(response);
   });
 });
+
+// var axios = require("axios");
+
+// $("#submit-search").on("click", function(e) {
+//   e.preventDefault();
+
+//   axios
+//     .get(
+//       "https://sandbox-api.brewerydb.com/v2/beers?key=7aed8b39d23007c4acc04ff75f4a0d6e"
+//     )
+//     .then(function(response) {
+//       // If the axios was successful...
+//       // Then log the body from the site!
+//       console.log(response.data);
+//     })
+//     .catch(function(error) {
+//       if (error.response) {
+//         // The request was made and the server responded with a status code
+//         // that falls out of the range of 2xx
+//         console.log(error.response.data);
+//         console.log(error.response.status);
+//         console.log(error.response.headers);
+//       } else if (error.request) {
+//         // The request was made but no response was received
+//         // `error.request` is an object that comes back with details pertaining to the error that occurred.
+//         console.log(error.request);
+//       } else {
+//         // Something happened in setting up the request that triggered an Error
+//         console.log("Error", error.message);
+//       }
+//       console.log(error.config);
+//     });
+// });
