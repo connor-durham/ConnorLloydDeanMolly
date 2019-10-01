9117814423a9fac8bb121748eb95a1f4eda772f7
Mnomens <48028258+Mnomens@users.noreply.github.com>
2019-10-01 16:30:34 -0400

    added changes

---
+++ models/seeds.sql
@@ -0,0 +1,3 @@
+USE projectTwodb;
+INSERT INTO Favorites (beerName, breweryName, webSite, image, createdAt, updatedAt)
+VALUES ('BudLight', 'Hops', 'testing.com', 'sometlehlejfl.png', '2019-10-1', '2019-10-1');
